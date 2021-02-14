8cbcc6d68d1516da759c6c7f67a5788683580700
jonathan <pvtang@foxmail.com>
2021-02-14 16:51:39 +0800

    add #797

---
+++ 797. 所有可能的路径.cpp
@@ -0,0 +1,43 @@
+// 797. 所有可能的路径
+// 给一个有 n 个结点的有向无环图，找到所有从 0 到 n-1 的路径并输出（不要求按顺序）
+// 二维数组的第 i 个数组中的单元都表示有向图中 i 号结点所能到达的下一些结点（译者注：有向图是有方向的，即规定了 a→b 你就不能从 b→a ）空就是没有下一个结点了。
+// 示例 1：
+// 输入：graph = [[1,2],[3],[3],[]]
+// 输出：[[0,1,3],[0,2,3]]
+// 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+// 示例 2：
+// 输入：graph = [[4,3,1],[3,2,4],[3],[4],[]]
+// 输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+// 示例 3：
+// 输入：graph = [[1],[]]
+// 输出：[[0,1]]
+// 示例 4：
+// 输入：graph = [[1,2,3],[2],[3],[]]
+// 输出：[[0,1,2,3],[0,2,3],[0,3]]
+// 示例 5
+// 输入：graph = [[1,3],[2],[3],[]]
+// 输出：[[0,1,2,3],[0,3]]
+// 提示：
+// 结点的数量会在范围 [2, 15] 内。
+// 你可以把路径以任意顺序输出，但在路径内的结点的顺序必须保证。
+#include<vector>
+using namespace std;
+vector<vector<int>>res;
+vector<vector<int>>allPathsSourceTarget(vector<vector<int>>&graph){
+    vector<int>track;
+    track.push_back(0);
+    back_track(graph,track,0);
+    return res;
+}
+void back_track(vector<vector<int>>&graph,vector<int>&track,int start){
+    int n=graph.size();
+    if(start=n-1){
+        res.push_back(track);
+        return ;
+    }
+    for(int i=0;i<graph[start].size();i++){
+        track.push_back(graph[start][i]);
+        back_track(graph,track,graph[start][i]);
+        track.pop_back();
+    }
+}
